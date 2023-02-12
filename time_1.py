48e6733ed0e5e8ab929b4c4bc38ef06bcbea87a0
AustinJM22 <a_medina11@u.pacific.edu>
2023-02-11 21:24:11 -0800

    Testing how to code in python

--- time_1.py
+++ time_1.py
@@ -1,6 +1,13 @@
 #our main time class
 def main():
     print("Hello World!")
+    name()
+    #Calling the function in main
+
+#Function
+def name():
+    print()
+    print("Testing Python")
 
 if __name__ == "__main__":
     main()
