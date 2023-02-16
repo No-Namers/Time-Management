b08e5e25b77fbd14bd5c3f7b5e8b29de10e61c43
nfantinodyer <n_fantinodyer@u.pacific.edu>
2023-02-15 16:05:37 -0800

    Import a simple calendar module that prints user date

--- time_1.py
+++ time_1.py
@@ -1,14 +1,26 @@
-#our main time class
+import calendar
+import datetime
+
 def main():
-    print("Hello World!")
-    name()
-    #Calling the function in main
+    print("Welcome to our calendar")
+    date=input("Enter the date in the format of mm/dd/yyyy: ")
+    cal(date)
 
 #Function
-def name():
-    print()
-    print("Testing Python")
-    print("bruh")
+def cal(date):
+    #define the calendar
+    textCal = calendar.TextCalendar(firstweekday=0)
+    
+    month,day,year=date.split("/")
+    month=int(month)
+    day=int(day)
+    year=int(year)
+
+    #print the calendar
+    textCal.formatmonth(year, month, w=0, l=0)
+    textCal.prmonth(year, month, w=0, l=0)
+
+    #print(calendar.calendar(year))
 
 if __name__ == "__main__":
     main()
