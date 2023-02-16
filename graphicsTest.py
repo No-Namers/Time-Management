76007b10bed638e93a616197e3702798c584574a
nfantinodyer <n_fantinodyer@u.pacific.edu>
2023-02-15 16:41:31 -0800

    changed colors and added line

--- graphicsTest.py
+++ graphicsTest.py
@@ -1,10 +1,20 @@
 from graphics import *
 
 def main():
-    win = GraphWin("My Circle", 400, 400)
+    win = GraphWin("My Window", 500, 500)
+    win.setBackground(color_rgb(0,0,0))
+
     c = Circle(Point(100, 100), 50)
+    c.setFill(color_rgb(255,0,0))
     c.draw(win)
-    win.getMouse() # Pause to view result
+
+    pt1=Point(100,150)
+    pt2=Point(150,200)
+    li=Line(pt1,pt2)
+    li.setOutline(color_rgb(0,255,255))
+    li.draw(win)
+
+    win.getMouse() 
     win.close()    # Close window when done
 
 main()
