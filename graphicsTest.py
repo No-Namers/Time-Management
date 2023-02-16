ef833b255fa697166657d360e184408077aa6d34
nfantinodyer <n_fantinodyer@u.pacific.edu>
2023-02-15 18:30:50 -0800

    we can run tests in this one

--- graphicsTest.py
+++ graphicsTest.py
@@ -1,20 +1,102 @@
+import calendar
 from graphics import *
+import datetime
 
 def main():
-    win = GraphWin("My Window", 500, 500)
-    win.setBackground(color_rgb(0,0,0))
+    win = GraphWin("Calendar", 500, 500)
+    #win.setBackground(color_rgb(0,0,0))
 
-    c = Circle(Point(100, 100), 50)
-    c.setFill(color_rgb(255,0,0))
-    c.draw(win)
+    pt=Point(0, 0)
 
-    pt1=Point(100,150)
-    pt2=Point(150,200)
-    li=Line(pt1,pt2)
-    li.setOutline(color_rgb(0,255,255))
-    li.draw(win)
+    exitrec=Rectangle(Point(450, 450), Point(490, 490))
+    exitrec.setFill(color_rgb(255,0,0))
+    exitrec.draw(win)
 
-    win.getMouse() 
-    win.close()    # Close window when done
+    exittext=Text(Point(470, 470), "Exit")
+    exittext.setTextColor(color_rgb(255,255,255))
+    exittext.draw(win)
 
-main()
+    searchrec=Rectangle(Point(10, 450), Point(70, 490))
+    searchrec.setFill(color_rgb(0,255,0))
+    searchrec.draw(win)
+
+    searchtext=Text(Point(40, 470), "Search")
+    searchtext.setTextColor(color_rgb(255,255,255))
+    searchtext.draw(win)
+
+    #create a Text object to hold the calendar output
+    calText = Text(Point(250, 250), "")
+    calText.setSize(14)
+    calText.setFace('courier')
+    calText.draw(win)
+
+    txt = Text(Point(250, 15), "Enter the date in the format of mm/dd/yyyy:")
+    txt.setTextColor(color_rgb(0,0,0))
+    txt.setSize(14)
+    txt.setFace('courier')
+    txt.draw(win)
+
+    input_box = Entry(Point(250, 50), 20)
+    input_box.draw(win)
+
+    txt=Text(Point(250, 100), "")
+    txt.draw(win)
+
+    while True:
+        click_point=win.getMouse()
+        if click_point.getX()>=450 and click_point.getX()<=490 and click_point.getY()>=450 and click_point.getY()<=490:
+            win.close()
+            break
+        txt.setText("")
+        date=input_box.getText()
+        if click_point.getX()>=10 and click_point.getX()<=70 and click_point.getY()>=450 and click_point.getY()<=490:
+            try:
+                month,day,year=date.split("/")
+                month=int(month)
+                day=int(day)
+                year=int(year)
+                if month>12 or month<1 or day>31 or day<1 or year>9999 or year<1:
+                    raise ValueError
+                else:
+                    calText.setText("")
+                    cal(date, win,calText)
+            except ValueError:
+                txt.setText("Invalid date. Please try again.")
+                input_box.setText("")
+    
+
+    click_point=win.getMouse()
+    if click_point.getX()>=450 and click_point.getX()<=490 and click_point.getY()>=450 and click_point.getY()<=490:
+        win.close()
+
+
+#Need to make days clickable for user to add events and tasks
+#i think we will have to print out parts of the calendar individually so that we can make the days clickable
+#not sure how to do this yet
+
+#or we could search if the user types in the full date (mm/dd/yyyy) and then print out the calendar for 
+#that month as well as the todo list for that day
+#this is probably the easiest way to do it
+#and if the user types in a month and year, we just can print out the calendar for that month
+def cal(date, win,calText):
+    #define the calendar
+    textCal = calendar.TextCalendar(firstweekday=0)
+    
+    month,day,year=date.split("/")
+    month=int(month)
+    day=int(day)
+    year=int(year)
+
+    
+
+    #set the text to the calendar output
+    calOutput = textCal.formatmonth(year, month, w=0, l=0)
+    calText.setText(calOutput)
+
+
+def showToDoList(date, win):
+    #That day's to do list
+    print("To Do List for " + date + ":")
+
+if __name__ == "__main__":
+    main()
