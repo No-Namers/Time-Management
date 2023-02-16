01a5aa345f11bc3e328952dcef9a941500fd93d2
nfantinodyer <n_fantinodyer@u.pacific.edu>
2023-02-16 13:42:58 -0800

    shows date at the top of the todolist

--- todoListPane.py
+++ todoListPane.py
@@ -17,6 +17,32 @@ def showtodoListPane(win, date):
     exittext.setTextColor(color_rgb(255,255,255))
     exittext.draw(win)
 
+    createTaskrec=Rectangle(Point(10, 450), Point(100, 490))
+    createTaskrec.setFill(color_rgb(0,255,0))
+    createTaskrec.draw(win)
+
+    createTasktext=Text(Point(55, 470), "Create Task")
+    createTasktext.setTextColor(color_rgb(255,255,255))
+    createTasktext.draw(win)
+
+    #shows date at the top
+    dateText = Text(Point(250, 15), date)
+    dateText.setSize(14)
+    dateText.setFace('courier')
+    dateText.draw(win)
+
+    while True:
+        click_point=win.getMouse()
+        if click_point.getX()>=450 and click_point.getX()<=490 and click_point.getY()>=450 and click_point.getY()<=490:
+            win.close()
+            break
+        elif click_point.getX()>=10 and click_point.getX()<=100 and click_point.getY()>=450 and click_point.getY()<=490:
+            createTask(win, date)
+
     click_point=win.getMouse()
     if click_point.getX()>=450 and click_point.getX()<=490 and click_point.getY()>=450 and click_point.getY()<=490:
         win.close()
+
+def createTask(win, date):
+    #this will be where the user can create a task
+    return 0
