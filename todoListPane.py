1f3d7f65d7bbe5f6d62377cee223377192afdd21
nfantinodyer <n_fantinodyer@u.pacific.edu>
2023-02-16 17:28:20 -0800

    added file writing

--- todoListPane.py
+++ todoListPane.py
@@ -31,10 +31,57 @@ def showtodoListPane(date):
     dateText.setFace('courier')
     dateText.draw(win)
 
+    readTasks(win, date)
+
     click_point=win.getMouse()
     if click_point.getX()>=450 and click_point.getX()<=490 and click_point.getY()>=450 and click_point.getY()<=490:
         win.close()
+    elif click_point.getX()>=10 and click_point.getX()<=100 and click_point.getY()>=450 and click_point.getY()<=490:
+        createTask(win, date)
 
 def createTask(win, date):
-    #this will be where the user can create a task
-    return 0
+    #create a Text object to hold the calendar output
+    taskText = Text(Point(250, 250), "")
+    taskText.setSize(14)
+    taskText.setFace('courier')
+    taskText.draw(win)
+
+    txt = Text(Point(250, 15), "Enter the task:")
+    txt.setTextColor(color_rgb(0,0,0))
+    txt.setSize(14)
+    txt.setFace('courier')
+    txt.draw(win)
+
+    input_box = Entry(Point(250, 50), 20)
+    input_box.draw(win)
+
+    txt=Text(Point(250, 100), "")
+    txt.draw(win)
+
+    while True:
+        click_point=win.getMouse()
+        if click_point.getX()>=450 and click_point.getX()<=490 and click_point.getY()>=450 and click_point.getY()<=490:
+            win.close()
+            break
+        elif click_point.getX()>=10 and click_point.getX()<=100 and click_point.getY()>=450 and click_point.getY()<=490:
+            taskText.setText(input_box.getText())
+            #save task to file
+            file = open("todoList.txt", "a")
+            file.write(date + " " + input_box.getText()+"\n")
+            file.close()
+            break
+
+def readTasks(win, date):
+    #create a Text object to hold the calendar output
+    taskText = Text(Point(250, 250), "")
+    taskText.setSize(14)
+    taskText.setFace('courier')
+    taskText.draw(win)
+
+    file = open("todoList.txt", "r")
+    tasks = file.read()
+    file.close()
+
+    tasks = tasks.split("\n")
+
+    
